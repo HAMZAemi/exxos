36be5953636289dacdad3bafd932a8c43479e080
Hamza El khadri <95679729+HAMZAemi@users.noreply.github.com>
2023-07-22 23:40:09 +0100

    Update Example.java

--- src/main/java/com/exosdata/data/backendexample/entities/Example.java
+++ src/main/java/com/exosdata/data/backendexample/entities/Example.java
@@ -1,6 +1,8 @@
 package com.exosdata.data.backendexample.entities;
-
-import javax.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
